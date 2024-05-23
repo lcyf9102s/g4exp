a323ded28ab277e7bdddcdfe847ee4d2cb22aef1
lcyf9102s <maxliu9911@protonmail.ch>
2024-05-23 21:06:51 +0800

    添加切伦科夫辐射相关的参数设置

--- src/construction.cc
+++ src/construction.cc
@@ -12,11 +12,9 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
     // define materials
     G4NistManager *nist = G4NistManager::Instance();
 
-    // World
+    // World材质
     G4Material *worldMat = nist->FindOrBuildMaterial("G4_AIR"); // ROI(boundary) & material
-    G4Box *solidWorld = new G4Box("solidWorld", 0.5*m, 0.5*m, 0.5*m); // solid world
-    G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld"); // logical world
-    G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true); // physical world
+    
     
     // 设置探测器材料构成：气凝胶（aerogel）SiO2, H2O, C
     G4Material *SiO2 = new G4Material("SiO2", 2.201*g/cm3, 2);
@@ -34,9 +32,28 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
     Aerogel->AddMaterial(H2O, 37.4*perCent);
     Aerogel->AddElement(C, 0.1*perCent);
 
+    // 设置切伦科夫辐射相关参数，momentum动量、refractive index(?)反射系数（要显示可见光光子，必须要设定材料的反射系数）
+    G4double energy[2] = {1.239841939*eV/0.9, 1.239841939*eV/0.2};
+    G4double rindexAerogel[2] = {1.1, 1.1};
+    G4MaterialPropertiesTable *mptAerogel = new G4MaterialPropertiesTable();
+    mptAerogel->AddProperty("RINDEX", energy, rindexAerogel, 2);
+    Aerogel->SetMaterialPropertiesTable(mptAerogel);
+
+    // 设置world的反射系数，以显示光路
+    G4double rindexWorld[2] = {1.0, 1.0};
+    G4MaterialPropertiesTable *mptWorld = new G4MaterialPropertiesTable();
+    mptWorld->AddProperty("RINDEX", energy, rindexWorld, 2);
+    worldMat->SetMaterialPropertiesTable(mptWorld);
+
+    // world和探测器几何设置
+    G4Box *solidWorld = new G4Box("solidWorld", 0.5*m, 0.5*m, 0.5*m); // solid world
+    G4LogicalVolume *logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld"); // logical world
+    G4VPhysicalVolume *physWorld = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), logicWorld, "physWorld", 0, false, 0, true); // physical world
+
     G4Box *solidRadiator = new G4Box("solidRadiator", 0.4*m, 0.4*m, 0.01*m);
     G4LogicalVolume *logicRadiator = new G4LogicalVolume(solidRadiator, Aerogel, "logicRadiator");
     G4VPhysicalVolume *physRadiator = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.2*m), logicRadiator, "physRadiator", logicWorld, false, 0, true);
+
 
     return physWorld;
 }
